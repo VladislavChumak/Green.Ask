9f14713fd795df70828a9ca1e91bfec54b41166b
Vl4D0SS <57459653+Vl4D0SS@users.noreply.github.com>
2022-11-27 04:38:01 +0300

    test

--- GreenAtom/src/main/java/com/boots/controller/QuizControler.java
+++ GreenAtom/src/main/java/com/boots/controller/QuizControler.java
@@ -5,22 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-public class SelectControler {
+public class QuizControler {
     @Autowired
     private UserService userService;
-    
-    @GetMapping("/select/{name}")
-    public String selectVacancy(@PathVariable(name = "name", required = true) String vacancy_name, Model model) {
-        return "redirect:/quiz?name="+vacancy_name;
-    }
-    
-    @GetMapping(value = "/isVacancyActive/{name}")
-    public ResponseEntity<?> isVacancyActive(@PathVariable(name = "name", required = true) String vacancy_name) {
+
+    @GetMapping(value = "quiz/{task}/{qestion}")
+    public ResponseEntity<?> isVacancyActive(@PathVariable(name = "task", required = true) String vacancy_name) {
     	return userService.isVacancyActive(vacancy_name) ? new ResponseEntity<>(HttpStatus.OK) : new ResponseEntity<>(HttpStatus.FOUND);
     }
 }
